408c569adba023bc7157b4254e587bc5d7416414
Resonaura <resonaura@gmail.com>
2024-02-02 20:23:56 +0100

    Fixes

--- ui/electron/index.js
+++ ui/electron/index.js
@@ -1,7 +1,18 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 // Get the port number from command line arguments
 const port = process.argv[2] || '2811'; // Default to 2811 if no argument is provided
+
+let iconPath;
+
+if (process.platform === 'win32') {
+  iconPath = path.join(__dirname, 'assets/icons/resobox.ico');
+} else if (process.platform === 'darwin') {
+  iconPath = path.join(__dirname, 'assets/icons/resobox.icns');
+} else if (process.platform === 'linux') {
+  iconPath = path.join(__dirname, 'assets/icons/resobox.png');
+}
 
 function createWindow() {
   // Create the browser window.
@@ -10,7 +21,8 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    icon: iconPath,
   });
 
   // Construct the URL with the port
